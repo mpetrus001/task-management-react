1e1337ea62fa2e4d4a597d06a4c20b248e4c0376
MatthewP <mpetrus001@gmail.com>
2020-10-26 10:06:49 -0500

    initial commit after CRA

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <p>Hello, React!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
